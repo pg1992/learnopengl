d48e38ac5948391ee68683f4d21345badbf26f60
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2026-06-21 18:10:36 -0300

    Play with background colors

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+const int TOTAL_COLORS = 4;
+GLuint color_index = 0;
+GLfloat colors[TOTAL_COLORS][3] = {
+    {.0, .5, .5},
+    {.5, .0, .5},
+    {.5, .5, .0},
+    {.5, .5, .5},
+};
+
 int main()
 {
     glfwInit();
@@ -40,9 +49,13 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        float r = colors[color_index][0];
+        float g = colors[color_index][1];
+        float b = colors[color_index][2];
+
         glfwPollEvents();
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(r, g, b, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
@@ -56,6 +69,8 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
+    else if (key == GLFW_KEY_J && action == GLFW_PRESS)
+        color_index = (color_index + 1) % TOTAL_COLORS;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
