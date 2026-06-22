71aacb83c90b0d9dea42b946f6732e6a7ecfc953
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2026-06-22 19:02:01 -0300

    Send variable from the vertex shader to the fragment shader

--- src/10-shader.cpp
+++ src/10-shader.cpp
@@ -21,17 +21,20 @@ GLfloat colors[TOTAL_COLORS][3] = {
 const char *vertexShaderSource =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
 
 const char *fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "    FragColor = vertexColor;\n"
     "}\0";
 
 int main()
