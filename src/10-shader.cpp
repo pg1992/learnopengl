a7d517411e15b50e959300cb3c396c21c5df50ad
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2026-06-22 21:34:07 -0300

    Pass the color data

--- src/10-shader.cpp
+++ src/10-shader.cpp
@@ -13,27 +13,30 @@ const GLuint HEIGHT = 600;
 const int TOTAL_COLORS = 4;
 GLuint color_index = 0;
 GLfloat colors[TOTAL_COLORS][3] = {
-    {.0, .5, .5},
-    {.5, .0, .5},
-    {.5, .5, .0},
-    {.5, .5, .5},
+    {1., .0, .0},
+    {.0, 1., .0},
+    {.0, .0, 1.},
+    {.1, .1, .1},
 };
 
 const char *vertexShaderSource =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "    gl_Position = vec4(aPos, 1.0);\n"
+    "    ourColor = aColor;\n"
     "}\0";
 
 const char *fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
+    "in  vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "    FragColor = ourColor;\n"
+    "    FragColor = vec4(ourColor, 1.0);\n"
     "}\0";
 
 int main()
@@ -120,9 +123,10 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left
-         0.5f, -0.5f, 0.0f, // right
-         0.0f,  0.5f, 0.0f, // top
+        // positions        // colors
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // top
     };
 
     GLuint vao, vbo;
@@ -134,8 +138,12 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -166,12 +174,6 @@ int main()
 
         // be sure to activate the shader
         glUseProgram(shaderProgram);
-
-        // update the uniform color
-        float timeValue = glfwGetTime();
-        float greenValue = (std::sin(2 * M_PI * timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
         // now render the triangle
         glBindVertexArray(vao);  // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
