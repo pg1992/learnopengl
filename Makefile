0d8449d44572ff37ee1006e46a3151264724fe44
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2026-06-27 00:04:06 -0300

    Add skeletons

--- Makefile
+++ Makefile
@@ -3,19 +3,21 @@ CFLAGS += -ggdb3 -Iinclude
 LIBS += -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm
 TARGETS = bin/00-triangle.elf bin/01-triangle.elf bin/02-triangle.elf \
           bin/03-triangle.elf bin/10-shader.elf bin/20-texture.elf    \
-          bin/4.1-texture.elf bin/4.2-texture.elf bin/5.1-transformations.elf
+          bin/4.1-texture.elf bin/4.2-texture.elf bin/5.1-transformations.elf \
+          bin/6.1.coordinate_systems.elf
 
 all: $(TARGETS)
 
-bin/00-triangle.elf         : src/00-triangle.cpp
-bin/01-triangle.elf         : src/01-triangle.cpp
-bin/02-triangle.elf         : src/02-triangle.cpp
-bin/03-triangle.elf         : src/03-triangle.cpp
-bin/10-shader.elf           : src/10-shader.cpp src/Shader.cpp
-bin/20-texture.elf          : src/20-texture.cpp src/Shader.cpp
-bin/4.1-texture.elf         : src/4.1-texture.cpp src/Shader.cpp
-bin/4.2-texture.elf         : src/4.2-texture.cpp src/Shader.cpp
-bin/5.1-transformations.elf : src/5.1-transformations.cpp src/Shader.cpp
+bin/00-triangle.elf            : src/00-triangle.cpp
+bin/01-triangle.elf            : src/01-triangle.cpp
+bin/02-triangle.elf            : src/02-triangle.cpp
+bin/03-triangle.elf            : src/03-triangle.cpp
+bin/10-shader.elf              : src/10-shader.cpp src/Shader.cpp
+bin/20-texture.elf             : src/20-texture.cpp src/Shader.cpp
+bin/4.1-texture.elf            : src/4.1-texture.cpp src/Shader.cpp
+bin/4.2-texture.elf            : src/4.2-texture.cpp src/Shader.cpp
+bin/5.1-transformations.elf    : src/5.1-transformations.cpp src/Shader.cpp
+bin/6.1.coordinate_systems.elf : src/6.1.coordinate_systems.cpp src/Shader.cpp
 
 $(TARGETS):
 	$(CC) $(LIBS) $(CFLAGS) src/gl.c $^ -o $@
---
+++ src/6.1.coordinate_systems.cpp
@@ -0,0 +1,233 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <Shader.h>
+
+#include <iostream>
+#include <cmath>
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+const GLuint WIDTH = 800;
+const GLuint HEIGHT = 600;
+
+float mixer = 0.2;
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    // glfw: window creation
+    // ---------------------
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    // build and compile our shader program
+    // ------------------------------------
+    Shader ourShader("src/6.1.coordinate_systems.vs", "src/6.1.coordinate_systems.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    GLfloat vertices[] = {
+        // positions         // texture coords
+         0.5f,  0.5f, 0.0f,  1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, // top left
+    };
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3,
+    };
+
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    const GLsizei stride = 5 * sizeof(float);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // set the texture wrapping/filtering options (on the currently bound
+    // texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("assets/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    data = stbi_load("assets/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
+
+    double initialTime = glfwGetTime();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // render
+        // ------
+
+        // clear the colorbuffer
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // be sure to activate the shader
+        ourShader.use();
+        ourShader.setFloat("mixer", mixer);
+        // now render the triangle
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        // create transformations
+        glm::mat4 transform = glm::mat4(1.0f);
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        ourShader.setMat4("transform", transform);
+        // render container
+        //glBindVertexArray(vao);  // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // second transformation
+        transform = glm::mat4(1.0f);
+        transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
+        double dt = glfwGetTime() - initialTime;
+        double scaleAmount = (std::sin(2 * M_PI * dt) + 1.0) / 2.0;
+        transform = glm::scale(transform, glm::vec3(scaleAmount));
+        ourShader.setMat4("transform", transform);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0);  // no need to unbind it every time
+
+        // glfw: swapp buffers and poll IO events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (action == GLFW_PRESS)
+    {
+        switch (key)
+        {
+            case GLFW_KEY_ESCAPE:
+            case GLFW_KEY_Q:
+                glfwSetWindowShouldClose(window, GL_TRUE);
+                break;
+            case GLFW_KEY_UP:
+                mixer = std::min(1.0, mixer + 0.1);
+                break;
+            case GLFW_KEY_DOWN:
+                mixer = std::max(0.0, mixer - 0.1);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
