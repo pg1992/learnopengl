3637f42a2d0a33300748f6a20a723423f71fc708
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2026-06-29 01:02:10 -0300

    Create the LookAt function

--- include/FPSCamera.h
+++ include/FPSCamera.h
@@ -63,7 +63,7 @@ public:
     // returns the view matrix calculated using Euler Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix()
     {
-        return glm::lookAt(Position, Position + Front, Up);
+        return LookAt(Position, Position + Front, Up);
     }
 
     // processes input received from any keyboard-like input system.  Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
@@ -126,6 +126,30 @@ private:
         // also re-calculate the Right and Up vector
         Right = glm::normalize(glm::cross(Front, WorldUp)); // normalize the vectors, because their length get closer to 0 the more you look up or down which results in slower movement.
         Up    = glm::normalize(glm::cross(Right, Front));
+    }
+
+    glm::mat4 LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
+    {
+        glm::vec3 zaxis = glm::normalize(position - target);
+        glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
+        glm::vec3 yaxis = glm::cross(zaxis, xaxis);
+
+        glm::mat4 translation = glm::mat4(1.0f);
+        translation[3][0] = -position.x;
+        translation[3][1] = -position.y;
+        translation[3][2] = -position.z;
+        glm::mat4 rotation = glm::mat4(1.0f);
+        rotation[0][0] = xaxis.x;
+        rotation[1][0] = xaxis.y;
+        rotation[2][0] = xaxis.z;
+        rotation[0][1] = yaxis.x;
+        rotation[1][1] = yaxis.y;
+        rotation[2][1] = yaxis.z;
+        rotation[0][2] = zaxis.x;
+        rotation[1][2] = zaxis.y;
+        rotation[2][2] = zaxis.z;
+
+        return rotation * translation;
     }
 };
 
